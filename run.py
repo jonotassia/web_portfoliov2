88ba9ed354bbc90eeead4b305e073365be0db8fa
Jono Tassia <jonotassia@gmail.com>
2023-04-25 09:33:41 +0100

    Formatting fixes

--- run.py
+++ run.py
@@ -114,7 +114,7 @@ def submit_form():
         return "Something went wrong."
 
 
-# if __name__ == "__main__":
-#     app.run(host='0.0.0.0', port=5000, debug=True)
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000, debug=True)
 
 # TODO: Add portfolio information to each portfolio item
